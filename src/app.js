78e26f65047f56557bbc3862a6d4f8511030d69a
unknown <mahmoudabdelnaby362001@gmail.com>
2022-09-28 10:05:32 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express=require('express')
 const { error } = require('console')
 // console.log(__dirname)
 const app=express()
+const port=process.env.PORT || 3000
 
 const publicDirPath=path.join(__dirname,'../public')
 
@@ -121,6 +122,6 @@ app.get('*',(req,res)=>{
     errormessage:'page not found'
   })
 })
-app.listen(3000,()=>{
-  console.log('Server is up on port 3000')
+app.listen(port,()=>{
+  console.log('Server is up on port '+ port)
 })
